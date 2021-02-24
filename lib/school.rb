fa9b013cff52f626fce5d1c799d400f6d5c6372a
Christopher Kim <chris.kim1990@gmail.com>
2021-02-23 19:38:02 -0500

    commit tree caa11a8c7ba93fdbe956d84d49c91b271cbf1bc7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+    attr_accessor :name, :roster
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster[grade]
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+            # binding.pry
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        @roster.sort.each do |grade, array|
+            sorted[grade] = array.sort
+        end
+        sorted
+    end
+end
